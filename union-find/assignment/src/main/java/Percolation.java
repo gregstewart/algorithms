b773632b54b36fa174c6fff2753c038e813156b0
Greg Stewart <gregs@tcias.co.uk>
2017-11-16 14:29:29 +0000

    Finished with ther precolation

--- union-find/assignment/src/main/java/Percolation.java
+++ union-find/assignment/src/main/java/Percolation.java
@@ -1,7 +1,7 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-  private int gridSize, virtualTop, virtualBottom;
+  private int size, gridSize, virtualTop, virtualBottom;
   private boolean[] grid;
   private WeightedQuickUnionUF weightedUnion;
 
@@ -10,7 +10,8 @@ public class Percolation {
     if (n <= 0) {
       throw new IllegalArgumentException("n must be either be equal to or greater than 1");
     }
-    gridSize = n;
+    size = n;
+    gridSize = n * n;
     grid = new boolean[gridSize];
     virtualTop = 0;
     virtualBottom = n * n + 1;
@@ -27,10 +28,20 @@ public class Percolation {
     }
 
     if(!isOpen(row, col)) {
-      if(gridSize == 1) {
-        weightedUnion.union(virtualBottom, flattenCoordinates(row, col));
+      int id = flattenCoordinates(row, col);
+
+      if(row == 1) {
+        weightedUnion.union(virtualTop, id);
       }
-      grid[flattenCoordinates(row, col)] = true;
+      if (row == size) {
+        weightedUnion.union(virtualBottom, id);
+      }
+      connectAdjacent(id, row + 1, col);
+      connectAdjacent(id, row - 1, col);
+      connectAdjacent(id, row, col - 1);
+      connectAdjacent(id, row, col + 1);
+
+      grid[id] = true;
     }
   }
   // is site (row, col) open?
@@ -42,7 +53,6 @@ public class Percolation {
     if (col <= 0) {
       throw new IllegalArgumentException("col must be either be equal to or greater than 1");
     }
-
     return grid[flattenCoordinates(row, col)];
   }
   // is site (row, col) full?
@@ -54,20 +64,49 @@ public class Percolation {
     if (col <= 0) {
       throw new IllegalArgumentException("col must be either be equal to or greater than 1");
     }
-    return true;
+
+    if (isOpen(row, col)) {
+      return weightedUnion.connected(virtualTop, flattenCoordinates(row, col));
+    }
+    return false;
   }
 
-  private int flattenCoordinates(int row, int col) {
-    return (gridSize * (row - 1)) + (col - 1);
+  // number of open sites
+  public int numberOfOpenSites() {
+    int count = 0;
+    
+    for (boolean item : grid) {
+      if (item == true) {
+        count++;
+      }
+    }
+    return count;
   }
-
-  // public     int numberOfOpenSites() {
-  //
-  // }       // number of open sites
 
   // does the system percolate?
   public boolean percolates() {
-    return weightedUnion.connected(virtualTop, virtualBottom);
+    boolean percolates = weightedUnion.connected(virtualTop, virtualBottom);
+    return percolates;
+  }
+
+  private int flattenCoordinates(int row, int col) {
+    if(row < 0 || row > gridSize || col < 0 || col > gridSize) {
+      throw new IndexOutOfBoundsException("Illegal parameter value: " + row + "," + col);
+    }
+    return (size * (row - 1)) + (col - 1);
+  }
+
+  private void connectAdjacent(int id, int i, int j) {
+    try {
+      if (isOpen(i, j)) {
+        int neighbouringIndex = flattenCoordinates(i, j);
+        weightedUnion.union(id, neighbouringIndex);
+      }
+    } catch (IndexOutOfBoundsException e) {
+        // don't connect field with field outside grid
+    } catch (IllegalArgumentException e) {
+        // don't connect field with field outside grid
+    }
   }
 
   //
