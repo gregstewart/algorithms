9084d7730fe41a54fa9e0883f09ac59b4986957e
Greg Stewart <gregs@tcias.co.uk>
2017-11-30 19:04:46 +0000

    Setup base classes and test classes for assignment

---
+++ bags-queues-and-stacks/assignment/src/main/java/Permutation.java
@@ -0,0 +1,5 @@
+public class Permutation {
+  public static void main(String[] args) {
+
+  }
+}
---
+++ bags-queues-and-stacks/assignment/src/main/java/RandomizedQueue.java
@@ -0,0 +1,41 @@
+public class RandomizedQueue<Item> implements Iterable<Item> {
+  // construct an empty randomized queue
+  public RandomizedQueue() {
+
+  }
+
+  // is the randomized queue empty?
+  public boolean isEmpty() {
+
+  }
+
+  // return the number of items on the randomized queue
+  public int size() {
+
+  }
+
+  // add the item
+  public void enqueue(Item item) {
+
+  }
+
+  // remove and return a random item
+  public Item dequeue() {
+
+  }
+
+  // return a random item (but do not remove it)
+  public Item sample() {
+
+  }
+
+  // return an independent iterator over items in random order
+  public Iterator<Item> iterator() {
+
+  }
+
+  // unit testing (optional)
+  public static void main(String[] args) {
+
+  }
+}
